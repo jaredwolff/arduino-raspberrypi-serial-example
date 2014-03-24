284d555b122ad17a9eccefb973ec571c493c7dd9
Jared Wolff <jaredwolff@me.com>
2014-03-24 15:11:21 -0700

    Send a response after the Arduino has completed its work.

--- Watering/Watering.ino
+++ Watering/Watering.ino
@@ -16,6 +16,8 @@ void loop() {
   if ( Serial.available() ) {
 
     byte command = Serial.read();
+    byte output[4];
+    byte numBytes = 0;
 
     switch( command ) {
 
@@ -25,13 +27,27 @@ void loop() {
           break;
       case RELAY_ON:
           digitalWrite(LED_PIN, HIGH);
+                    
+          // Prepping to send a response
+          output[0] = command;
+          output[1] = 0;
+          numBytes = 2;
           break;
       case RELAY_OFF:
           digitalWrite(LED_PIN, LOW);
+                    
+          // Prepping to send a response
+          output[0] = command;
+          output[1] = 0;
+          numBytes = 2;
           break;
       default:
           break;
 
     }
+
+    //Sending a response after the command has completed
+    Serial.write(output, numBytes);
+
   }
 }
